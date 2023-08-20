f2406f59d5ec1a779af115457e23b5ecd40daa38
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-08-20 00:25:15 -0400

    All tests passing

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+
+function writeCards(arr, keyword){
+    
+    let arr2 = [];
+    
+    for(let i = 0; i < arr.length; i++){
+        console.log(`Thank you, ${arr[i]}, for the wonderful ${keyword} gift!`);
+        debugger;
+        arr2.push(`Thank you, ${arr[i]}, for the wonderful ${keyword} gift!`);
+    }
+
+    return arr2;
+}
+
+// console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+
+function countDown(num){
+
+    let firstExec = true;
+    let i = num;
+    for(let i = num ; i >= 0; i--){
+        if(firstExec){  
+            console.log(i);
+            // console.log(`Expected countDown(${num}) to log ${num} first`);
+            firstExec = false;}
+        else {
+            console.log(i);
+            // console.log(`Expected countDown(${num}) to log ${i} after ${i+1}`);
+            }
+                           
+        debugger;
+    }
+    return true;
+}
+
+
+
+
